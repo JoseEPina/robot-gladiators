acf5d01ce2895c1427a2d8085a99d5af6106d3b5
JoseEPina <jose.edpina@gmail.com>
2021-08-06 23:18:40 -0600

    Multiple rounds Complete.

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,9 +5,6 @@
 //    * Defeat each enemy-robot
 // "LOSE" - Player robot's health is zero or less
 
-// Alert players that they are starting the round
-window.alert("Welcome to Robot Gladiators!");
-
 var fight = function (enemyName) {
    // Repeat and execute as long as the enemy-robot is alive
    while (playerHealth > 0 && enemyHealth > 0) {
@@ -51,7 +48,7 @@ var fight = function (enemyName) {
 
       // check player's health
       if (playerHealth <= 0) {
-         window.alert(playerName + "has died!");
+         window.alert(playerName + " has died!");
          break;
       } else {
          window.alert(playerName + " still has " + playerHealth + " health remaining.");
@@ -72,17 +69,23 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 for (var i = 0; i < enemyNames.length; i++) {
-   var pickedEnemyName = enemyNames[i];
-   enemyHealth = 50;
-   fight(pickedEnemyName);
+   if (playerHealth > 0) {
+      // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+      // Pick new enemy to fight based on the index of the enemyNames array
+      var pickedEnemyName = enemyNames[i];
+
+      // Reset enemyHealth before starting new fight
+      enemyHealth = 50;
+
+      // Use debugger to pause script from running and check what's going on at that moment in the code
+      // debugger;
+
+      // Pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+      fight(pickedEnemyName);
+   } else {
+      window.alert("You have lost your robot in battle! Game Over!");
+      break;
+   }
 }
-
-// // If player chooses to fight, then fight
-// if (promptFight === "fight" || promptFight === "FIGHT") {
-//    // Remove enemy's health by subtracting the amount set in the playerAttack variable
-//    enemyHealth = enemyHealth - playerAttack;
-
-//    // Log a resulting message to the console so we know that it worked.
-//    console.log(
-//       playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-//    );
