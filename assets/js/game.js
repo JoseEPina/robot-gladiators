1bd161827ddd17caf1460ce8292ed7ba89998d61
JoseEPina <jose.edpina@gmail.com>
2021-08-07 17:26:48 -0600

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,13 +21,16 @@ var fight = function (enemyName) {
          if (confirmSkip) {
             window.alert(playerName + " has decided to skip this fight. Goodbye!");
             // subtract money from playerMoney for skipping
-            playerMoney = playerMoney - 10;
+            playerMoney = Math.max(0, playerMoney - 10);
             console.log("playerMoney is now ", playerMoney);
             break;
          }
       }
-      // Remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+
+      // Generate random damage value based on player's attack power
+      var damage = randomNumber(playerAttack - 3, playerAttack);
+      enemyHealth = Math.max(0, enemyHealth - damage);
+
       console.log(
          playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
       );
@@ -40,8 +43,9 @@ var fight = function (enemyName) {
          window.alert(enemyName + " still has " + enemyHealth + " health left.");
       }
 
-      // Remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      var damage = randomNumber(enemyAttack - 3, enemyAttack);
+      playerHealth = Math.max(0, playerHealth - damage);
+
       console.log(
          enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
       );
@@ -90,8 +94,8 @@ var startGame = function () {
          // Pick new enemy to fight based on the index of the enemyNames array
          var pickedEnemyName = enemyNames[i];
 
-         // Reset enemyHealth before starting new fight
-         enemyHealth = 50;
+         // Reset enemyHealth between 40 and 60, using our new function.
+         enemyHealth = randomNumber(40, 60);
 
          // Use debugger to pause script from running and check what's going on at that moment in the code
          // debugger;
@@ -166,6 +170,13 @@ var shop = function () {
          break;
       }
    }
+};
+
+// Function to generate a random numeric value
+var randomNumber = function (min, max) {
+   var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+   return value;
 };
 
 var playerName = window.prompt("What is your robot's name?");
